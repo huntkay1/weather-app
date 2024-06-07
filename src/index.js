81cc7441bc2b38a2694994914617b5abc202343f
Kayla Hunt <huntkay1@msu.edu>
2024-06-07 16:04:57 -0400

    Fetch data from api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+async function fetchData() {
+    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=9db405c9b16d42809f511752240606&q=Paris', {mode: 'cors'});
+    const weatherData = await response.json();
+    console.log(weatherData);
+}
+
+fetchData();
+
