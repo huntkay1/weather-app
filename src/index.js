6684bfd3670b995bf60e56493055ab715396ded6
Kayla Hunt <huntkay1@msu.edu>
2024-06-10 16:45:40 -0400

    Add user input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import './style.css';
 
-async function fetchData() {
-    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=9db405c9b16d42809f511752240606&q=Paris', {mode: 'cors'});
+const locationInput = document.getElementById('location');
+const submitBttn = document.querySelector('button');
+
+submitBttn.addEventListener('click', (e) => {fetchData(e)});
+
+async function fetchData(e) {
+    e.preventDefault();
+    const location = locationInput.value;
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=9db405c9b16d42809f511752240606&q=${location}&days=7`, {mode: 'cors'});
     const weatherData = await response.json();
-    console.log(weatherData);
+    console.log(weatherData.forecast);
 }
 
 fetchData();
