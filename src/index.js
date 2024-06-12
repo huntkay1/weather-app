3a6dc73e0a38bd745ae0229052571ba39a33a2e4
Kayla Hunt <huntkay1@msu.edu>
2024-06-12 18:20:53 -0400

    Implement hourly API data

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const currentTemp = document.getElementById('current-temp');
 const sunrise = document.getElementById('sunrise-time');
 const sunset = document.getElementById('sunset-time');
 
+
 submitBttn.addEventListener('click', (e) => {
     const location = locationInput.value;
     e.preventDefault();
@@ -24,7 +25,7 @@ async function fetchData(location) {
     return weatherData
 }
 
-window.addEventListener('load', () => {fetchData("South Haven");});
+window.addEventListener('load', () => {fetchData("South Haven Michigan");});
 
 
 function updateUI(weatherData) {
@@ -32,15 +33,46 @@ function updateUI(weatherData) {
     const forecastData = weatherData.forecast;
     const currentData = weatherData.current;
 
-    locationName.innerHTML = locationData.name;
+    locationName.innerHTML = locationData.name + ', ' + locationData.region;
     todaysHighTemp.innerHTML = forecastData.forecastday[0].day.maxtemp_f;
     todaysLowTemp.innerHTML = forecastData.forecastday[0].day.mintemp_f;
     currentTemp.innerHTML = currentData.temp_f;
     sunrise.innerHTML = forecastData.forecastday[0].astro.sunrise;
     sunset.innerHTML = forecastData.forecastday[0].astro.sunset;
 
+    createHourlyForecastCards(forecastData);
 }
 
+function createHourlyForecastCards(forecastData) {
+    const hourlyWeatherData = forecastData.forecastday[0].hour;
+    
+    hourlyWeatherData.forEach(hour => {
+        const timeStamp = hour.time.split(' ')[1];
+        const temp = hour.temp_f;
+        const weatherCondition = hour.condition.text;
+
+        const hourlyForecastContainer = document.getElementById('hourly-forecast-container')
+        const hourlyCard = document.createElement('div');
+        const hourlyCardTime = document.createElement('p');
+        const hourlyCardIcon = document.createElement('div');
+        const hourlyCardTemp = document.createElement('p');
+
+        hourlyCard.classList.add('hour-card');
+        hourlyCardTime.classList.add('card-time');
+        hourlyCardTemp.classList.add('hour-card-temp');
+        
+        hourlyCardTime.innerHTML = timeStamp;
+        hourlyCardIcon.innerHTML = '<svg width="50px" height="50px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M512 704a192 192 0 1 0 0-384 192 192 0 0 0 0 384zm0 64a256 256 0 1 1 0-512 256 256 0 0 1 0 512zm0-704a32 32 0 0 1 32 32v64a32 32 0 0 1-64 0V96a32 32 0 0 1 32-32zm0 768a32 32 0 0 1 32 32v64a32 32 0 1 1-64 0v-64a32 32 0 0 1 32-32zM195.2 195.2a32 32 0 0 1 45.248 0l45.248 45.248a32 32 0 1 1-45.248 45.248L195.2 240.448a32 32 0 0 1 0-45.248zm543.104 543.104a32 32 0 0 1 45.248 0l45.248 45.248a32 32 0 0 1-45.248 45.248l-45.248-45.248a32 32 0 0 1 0-45.248zM64 512a32 32 0 0 1 32-32h64a32 32 0 0 1 0 64H96a32 32 0 0 1-32-32zm768 0a32 32 0 0 1 32-32h64a32 32 0 1 1 0 64h-64a32 32 0 0 1-32-32zM195.2 828.8a32 32 0 0 1 0-45.248l45.248-45.248a32 32 0 0 1 45.248 45.248L240.448 828.8a32 32 0 0 1-45.248 0zm543.104-543.104a32 32 0 0 1 0-45.248l45.248-45.248a32 32 0 0 1 45.248 45.248l-45.248 45.248a32 32 0 0 1-45.248 0z"/></svg>';
+        hourlyCardTemp.innerHTML = temp + '&#176';
+    
+    
+        hourlyCard.appendChild(hourlyCardTime);
+        hourlyCard.appendChild(hourlyCardIcon);
+        hourlyCard.appendChild(hourlyCardTemp);
+        hourlyForecastContainer.appendChild(hourlyCard);
+    })
+
+}
 
 
 
