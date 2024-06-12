cbe1a5082cce256cf23572b8d9cf525af9a8bba7
Kayla Hunt <huntkay1@msu.edu>
2024-06-12 19:09:28 -0400

    Format hour text

--- src/index.js
+++ src/index.js
@@ -44,7 +44,6 @@ function updateUI(weatherData) {
 
 function createHourlyForecastCards(forecastData) {
     const hourlyWeatherData = forecastData.forecastday[0].hour;
-    console.log(hourlyWeatherData);
     
     hourlyWeatherData.forEach(hour => {
         const timeStamp = hour.time.split(' ')[1];
@@ -60,11 +59,9 @@ function createHourlyForecastCards(forecastData) {
         hourlyCard.classList.add('hour-card');
         hourlyCardTime.classList.add('card-time');
         hourlyCardTemp.classList.add('hour-card-temp');
-        
-        const weatherIcon = getWeatherConditionIcon(weatherCondition);
 
-        hourlyCardTime.innerHTML = timeStamp;
-        hourlyCardIcon.innerHTML = weatherIcon;
+        hourlyCardTime.innerHTML = formatHour(timeStamp);
+        hourlyCardIcon.innerHTML = getWeatherConditionIcon(weatherCondition);
         hourlyCardTemp.innerHTML = temp + '&#176';
     
     
@@ -77,7 +74,6 @@ function createHourlyForecastCards(forecastData) {
 }
 
 function getWeatherConditionIcon(weatherCondition) {
-console.log(weatherCondition)
     const sunIcon = '<svg width="50px" height="50px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M512 704a192 192 0 1 0 0-384 192 192 0 0 0 0 384zm0 64a256 256 0 1 1 0-512 256 256 0 0 1 0 512zm0-704a32 32 0 0 1 32 32v64a32 32 0 0 1-64 0V96a32 32 0 0 1 32-32zm0 768a32 32 0 0 1 32 32v64a32 32 0 1 1-64 0v-64a32 32 0 0 1 32-32zM195.2 195.2a32 32 0 0 1 45.248 0l45.248 45.248a32 32 0 1 1-45.248 45.248L195.2 240.448a32 32 0 0 1 0-45.248zm543.104 543.104a32 32 0 0 1 45.248 0l45.248 45.248a32 32 0 0 1-45.248 45.248l-45.248-45.248a32 32 0 0 1 0-45.248zM64 512a32 32 0 0 1 32-32h64a32 32 0 0 1 0 64H96a32 32 0 0 1-32-32zm768 0a32 32 0 0 1 32-32h64a32 32 0 1 1 0 64h-64a32 32 0 0 1-32-32zM195.2 828.8a32 32 0 0 1 0-45.248l45.248-45.248a32 32 0 0 1 45.248 45.248L240.448 828.8a32 32 0 0 1-45.248 0zm543.104-543.104a32 32 0 0 1 0-45.248l45.248-45.248a32 32 0 0 1 45.248 45.248l-45.248 45.248a32 32 0 0 1-45.248 0z"/></svg>';
     const cloudIcon = '<svg width="50px" height="50px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M737.216 357.952 704 349.824l-11.776-32a192.064 192.064 0 0 0-367.424 23.04l-8.96 39.04-39.04 8.96A192.064 192.064 0 0 0 320 768h368a207.808 207.808 0 0 0 207.808-208 208.32 208.32 0 0 0-158.592-202.048zm15.168-62.208A272.32 272.32 0 0 1 959.744 560a271.808 271.808 0 0 1-271.552 272H320a256 256 0 0 1-57.536-505.536 256.128 256.128 0 0 1 489.92-30.72z"/></svg>';
     const partlyCloudIcon = '<svg width="50px" height="50px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M598.4 895.872H328.192a256 256 0 0 1-34.496-510.528A352 352 0 1 1 598.4 895.872zm-271.36-64h272.256a288 288 0 1 0-248.512-417.664L335.04 445.44l-34.816 3.584a192 192 0 0 0 26.88 382.848z"/><path fill="#000000" d="M139.84 501.888a256 256 0 1 1 417.856-277.12c-17.728 2.176-38.208 8.448-61.504 18.816A192 192 0 1 0 189.12 460.48a6003.84 6003.84 0 0 0-49.28 41.408z"/></svg>';
@@ -91,6 +87,22 @@ console.log(weatherCondition)
     } else if (weatherCondition === 'Patchy rain nearby') {
         return rainIcon;
     }
+}
+
+//reformats from military time
+function formatHour(hour) {
+    hour = hour.slice(0,2); //get first two digits 
+    if (hour === '00') {
+        hour = '12'
+    }
+    if (hour[0] === "0") {
+        hour = hour.substring(1); //gets rid of 0 at the front
+    }
+
+    const amOrPm = hour < 13 ? "AM" : "PM"; //since the hours end at 11PM
+	let hourNumber = hour < 12 ? hour : hour - 12;
+	hourNumber === 0 ? (hourNumber = 12) : null;
+	return `${hourNumber} ${amOrPm}`;
 }
 
 
