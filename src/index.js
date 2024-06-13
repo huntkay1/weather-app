3c913d8d6934380f7c372f5ea37f2066ce057fff
Kayla Hunt <huntkay1@msu.edu>
2024-06-12 23:34:35 -0400

    Update styling

--- src/index.js
+++ src/index.js
@@ -86,6 +86,7 @@ function createHourlyForecastCards(forecastData, sunriseTime, sunsetTime) {
     const hourlyWeatherData = forecastData.forecastday[0].hour;
     const hourlyForecastContainer = document.getElementById('hourly-forecast-container')
     hourlyForecastContainer.innerHTML = '';
+    console.log(hourlyWeatherData);
     
     hourlyWeatherData.forEach(hour => {
         const timeStamp = hour.time.split(' ')[1];
@@ -128,6 +129,7 @@ function createDailyForecastCards(forecastData) {
         const highTemp = currentTempUnit === 'F' ? weekday.day.maxtemp_f : weekday.day.maxtemp_c;
         const lowTemp = currentTempUnit === 'F' ? weekday.day.mintemp_f : weekday.day.mintemp_c;
         const weatherCondition = weekday.day.condition.text;
+        console.log(weatherCondition);
 
         const dayCard = document.createElement('div');
         const dayCardIcon = document.createElement('div');
@@ -172,12 +174,14 @@ function getWeatherConditionIcon(weatherCondition, formattedHour, sunsetTime, su
     const stormIcon = '<svg width="50px" height="50px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M288 671.36v64.128A239.808 239.808 0 0 1 63.744 496.192a240.32 240.32 0 0 1 199.488-236.8 256.128 256.128 0 0 1 487.872-30.976A256.064 256.064 0 0 1 736 734.016v-64.768a192 192 0 0 0 3.328-377.92l-35.2-6.592-12.8-33.408a192.064 192.064 0 0 0-365.952 23.232l-9.92 40.896-41.472 7.04a176.32 176.32 0 0 0-146.24 173.568c0 91.968 70.464 167.36 160.256 175.232z"/><path fill="#000000" d="M416 736a32 32 0 0 1-27.776-47.872l128-224a32 32 0 1 1 55.552 31.744L471.168 672H608a32 32 0 0 1 27.776 47.872l-128 224a32 32 0 1 1-55.68-31.744L552.96 736H416z"/></svg>';
     const nightIcon = '<svg width="50px" height="50px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M240.448 240.448a384 384 0 1 0 559.424 525.696 448 448 0 0 1-542.016-542.08 390.592 390.592 0 0 0-17.408 16.384zm181.056 362.048a384 384 0 0 0 525.632 16.384A448 448 0 1 1 405.056 76.8a384 384 0 0 0 16.448 525.696z"/></svg>';
 
-    if (weatherCondition === 'Sunny' || weatherCondition === 'Clear ') {
+    if (weatherCondition === 'Sunny' || weatherCondition === 'Clear ' || weatherCondition === 'Clear') {
         return sunIcon;
-    } else if (weatherCondition === 'Partly Cloudy ') {
+    } else if (weatherCondition === 'Partly Cloudy ' || weatherCondition === 'Patchy rain nearby') {
         return partlyCloudIcon;
-    } else if (weatherCondition === 'Patchy rain nearby' || weatherCondition === 'Moderate rain') {
+    } else if ( weatherCondition === 'Moderate rain' || weatherCondition === 'Patchy light drizzle' || weatherCondition === 'Light rain shower' || weatherCondition === 'Mist' || weatherCondition === 'Light rain') {
         return rainIcon;
+    } else if (weatherCondition === 'Overcast') {
+        return cloudIcon;
     };
 }
 
